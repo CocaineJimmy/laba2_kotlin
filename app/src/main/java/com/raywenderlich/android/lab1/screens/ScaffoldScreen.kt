bfaef079b0e140a8d043ffb6548897b22a83ab0c
sadar <sadarqsa@gmail.com>
2024-03-21 11:26:07 +0300

    Create ScaffoldScreen.kt

---
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -0,0 +1,25 @@
+package com.raywenderlich.android.lab1.screens
+
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScaffoldScreen() {
+
+}
+
+@Composable
+fun MyScaffold() {
+
+}
+
+@Composable
+fun MyTopAppBar(scaffoldState: ScaffoldState) {
+
+}
+
+@Composable
+fun MyBottomAppBar() {
+
+}
